28f5fbc16d90b6b1136cd91e958338601306bace
kqarlos <kqarlos@hotmail.com>
2023-12-08 01:27:51 -0800

    adds data. Calculates portfolio value

--- client/src/components/Portfolio/portfolio.js
+++ client/src/components/Portfolio/portfolio.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Asset from "../Asset/asset";
 
 function Portfolio({ portfolio }) {
 
+    const [data, setData] = useState({
+        fiatValue: portfolio.fiat.reduce(
+            (acc, asset) => {
+                return acc + asset.locations.reduce(
+                    (acc2, custodian) => {
+                        return acc2 + (custodian.qty * asset.priceAction[0].price)
+                    }
+                    , 0)
+            }, 0),
+        stockValue: portfolio.stocks.reduce(
+            (acc, asset) => {
+                return acc + asset.locations.reduce(
+                    (acc2, custodian) => {
+                        return acc2 + (custodian.qty * asset.priceAction[0].price)
+                    }
+                    , 0)
+            }, 0),
+        cryptoValue: portfolio.crypto.reduce(
+            (acc, asset) => {
+                return acc + asset.locations.reduce(
+                    (acc2, custodian) => {
+                        return acc2 + (custodian.qty * asset.priceAction[0].price)
+                    }
+                    , 0)
+            }, 0)
+    })
 
     return (
         <>
-            <div className="row justify-content-around bg-primary py-2 my-2 rounded-3">
-                <div className="text-white h3">Fiat</div>
-                {portfolio.fiat.map((n, i) => {
-                    return (
-                        <Asset key={i} asset={n} />
+            <div>
+                <div className="row justify-content-around bg-primary py-2 my-2 rounded-3">
+                    <div className="text-white h3">Fiat ${data.fiatValue}</div>
+                    {portfolio.fiat.map((n, i) => {
+                        return (
+                            <Asset key={i} asset={n} />
 
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
-            <div className="row justify-content-around bg-primary py-2 my-2 rounded-3">
-                <div className="text-white h3">Crypto</div>
-                {portfolio.crypto.map((n, i) => {
-                    return (
-                        <Asset key={i} asset={n} />
+            <div>
+                <div className="row justify-content-around bg-primary py-2 my-2 rounded-3">
+                    <div className="text-white h3">Crypto ${data.cryptoValue}</div>
+                    {portfolio.crypto.map((n, i) => {
+                        return (
+                            <Asset key={i} asset={n} />
 
-                    );
-                })}
+                        );
+                    })}
+                </div>
+            </div>
+            <div>
+                <div className="row justify-content-around bg-primary py-2 my-2 rounded-3">
+                    <div className="text-white h3">Stocks ${data.stockValue}</div>
+                    {portfolio.stocks.map((n, i) => {
+                        return (
+                            <Asset key={i} asset={n} />
+
+                        );
+                    })}
+                </div>
             </div>
         </>
 
